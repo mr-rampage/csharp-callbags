d24db52e46708283b6ee32255f5dd1d9dc186e0d
mr-rampage <readonly@che.wbac.ca>
2020-09-21 18:46:58 -0400

    refactor - Created BaseOperator to remove boilerplate

---
+++ Callbags/Operator/BaseOperator.cs
@@ -0,0 +1,46 @@
+﻿namespace Callbags.Operator
+{
+    internal abstract class BaseOperator<TInput, TOutput>: IOperator<TInput, TOutput>
+    {
+        protected ISource<TInput> Source { get; private set; }
+        protected ISink<TOutput> Sink { get; private set; }
+        
+        public virtual void Acknowledge(in ISource<TInput> source)
+        {
+            Source = source;
+        }
+
+        public abstract void Deliver(in TInput data);
+
+        public virtual void Complete()
+        {
+            Sink.Complete();
+        }
+
+        public virtual void Error<TError>(in TError error)
+        {
+            Sink.Error(error);
+        }
+
+        public virtual void Greet(in ISink<TOutput> sink)
+        {
+            Sink = sink;
+            Sink.Acknowledge(this);
+        }
+
+        public virtual void Request()
+        {
+            Source.Request();
+        }
+
+        public virtual void Terminate()
+        {
+            Source.Terminate();
+        }
+
+        public virtual void Terminate<TError>(in TError error)
+        {
+            Source.Terminate(error);
+        }
+    }
+}
--- Callbags/Operator/Map.cs
+++ Callbags/Operator/Map.cs
@@ -2,10 +2,8 @@
 
 namespace Callbags.Operator
 {
-    internal sealed class Map<TInput, TOutput>: IOperator<TInput, TOutput>
+    internal sealed class Map<TInput, TOutput>: BaseOperator<TInput, TOutput>
     {
-        private ISource<TInput> _source;
-        private ISink<TOutput> _sink;
         private readonly Func<TInput, TOutput> _transformation;
 
         public Map(in Func<TInput, TOutput> transformation)
@@ -13,45 +11,9 @@ namespace Callbags.Operator
             _transformation = transformation;
         }
         
-        public void Acknowledge(in ISource<TInput> source)
+        public override void Deliver(in TInput data)
         {
-            _source = source;
-        }
-
-        public void Deliver(in TInput data)
-        {
-            _sink.Deliver(_transformation(data));
-        }
-
-        public void Complete()
-        {
-            _sink.Complete();
-        }
-
-        public void Error<TError>(in TError error)
-        {
-            _sink.Error(error);
-        }
-
-        public void Greet(in ISink<TOutput> sink)
-        {
-            _sink = sink;
-            _sink.Acknowledge(this);
-        }
-
-        public void Request()
-        {
-            _source.Request();
-        }
-
-        public void Terminate()
-        {
-            _source.Terminate();
-        }
-
-        public void Terminate<TError>(in TError error)
-        {
-            _source.Terminate(error);
+            Sink.Deliver(_transformation(data));
         }
     }
 }
